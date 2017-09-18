8f5331b40baed1d85652ded70c444e0c5bcdb494
Roman Husar <romaogcsm@gmail.com>
2017-09-18 03:22:54 +0200

    correct sorting

--- src/reducers/profile.js
+++ src/reducers/profile.js
@@ -4,14 +4,14 @@ const user = (state={}, action) => {
       console.log('started loading userData')
       break;
     case 'LOADED_USER_PROFILE':
-      let selected = []
-      selected.push(action.data.metrics[0])
       let user = action.data
       user.metrics = user.metrics
         .sort((a,b)=> a.year -b.year)
       user.metrics.map(metrics => {
        return (metrics.percent = metrics.percent * 100) && (metrics.grow_units = metrics.grow_units * 100)
       })
+      let selected = []
+      selected.push(user.metrics[0])
       return {...state,  user, selected} 
     case 'LOADING_USER_PROFILE_FAILED':
       return {...state,  error:action.data} 
